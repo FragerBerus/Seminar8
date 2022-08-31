ffb752fce5c470f47f9e1bcd45deca0d8b66dd76
Berus Frager <gitmailru@yandex.ru>
2022-08-31 10:36:14 +0300

    Коммит файлов папки проекта Example055

---
+++ Example055/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 55:
+// Задайте двумерный массив. Напишите программу, которая заменяет
+// строки на столбцы. В случае, если это невозможно, программа
+// должна вывести сообщение для пользователя.
+Console.Clear();
+Console.Write("Задайте количество строк матрицы: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте количество столбцов матрицы: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+/// Метод заполнения матрицы GetArray
+int[,] GetArray(int m, int n)
+{
+    int[,] matrix = new int [m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(11);
+        }
+    }
+    return matrix;
+}
+/// Метод печати матрицы
+void PrintArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] ChangeRowsColumns (int[,] matrix)
+{
+    int[,] result = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int l = 0; l < matrix.GetLength(0); l++)
+    {
+        for (int k = 0; k < matrix.GetLength(1); k++)
+        {           
+            result[k, l] = matrix [l, k];
+        }
+    }
+    return result;
+}
+int[,] resultMatrix = GetArray(rows, columns);
+PrintArray(resultMatrix);
+Console.WriteLine("Модифицированная матрица:");
+PrintArray(ChangeRowsColumns(resultMatrix));
